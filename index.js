31d309746f506a1d952e1ccf1727e144ac5316e5
fabioued10 <fabioued10@gmail.com>
2020-06-29 11:22:42 +0200

    removing the xpack and importing the services instead of loading them in the init function

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 /*
  * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
@@ -14,11 +13,12 @@
  * permissions and limitations under the License.
  */
 
-import { i18n } from '@kbn/i18n';
+import { DEFAULT_APP_CATEGORIES } from '../../src/core/utils';
+import { GenerateReportService, SetupService, DownloadService, RecentCsvService } from './server/services';
 
 import ReportingRoute from './server/routes/reporting.js';
 
-export default function(kibana) {
+export default function (kibana) {
   return new kibana.Plugin({
     name: 'reporting',
     uiExports: {
@@ -26,9 +26,10 @@ export default function(kibana) {
         title: 'Reporting',
         description: 'Reporting Module',
         main: 'plugins/reporting/app',
+        category: DEFAULT_APP_CATEGORIES.kibana,
       },
     },
-    require:  ['kibana', 'elasticsearch'],
+    require: ['kibana', 'elasticsearch'],
 
     config(Joi) {
       return Joi.object({
@@ -38,57 +39,20 @@ export default function(kibana) {
     },
 
     init(server, options) {
-      const esDriver                = server.plugins.elasticsearch;
-      const esServer                = server.plugins;
-      const GenerateReportService   = require('./server/services/GenerateService');
-      const SetupService            = require('./server/services/SetupService');
-      const DownloadService         = require('./server/services/DownloadService');
-      const RecentCsvService        = require('./server/services/RecentCsvService');
-      const reportingService        = new GenerateReportService(esDriver, esServer);
-      const setupService            = new SetupService(esDriver, esServer);
-      const downloadService         = new DownloadService(esDriver, esServer);
-      const recentReportService     = new RecentCsvService(esDriver, esServer);
-      const services                = { reportingService, setupService, downloadService, recentReportService };
+      const esDriver = server.plugins.elasticsearch;
+      const esServer = server.plugins;
+      const reportingService = new GenerateReportService(esDriver, esServer);
+      const setupService = new SetupService(esDriver, esServer);
+      const downloadService = new DownloadService(esDriver, esServer);
+      const recentReportService = new RecentCsvService(esDriver, esServer);
+      const services = { reportingService, setupService, downloadService, recentReportService };
 
       server.injectUiAppVars('reporting', () => {
         const config = server.config();
         return {
-          backendRole: config.get('reporting.required_backend_role')
+          backendRole: config.get('reporting.required_backend_role'),
         };
       });
-      const xpackMainPlugin = server.plugins.xpack_main;
-      if (xpackMainPlugin) {
-        const featureId = 'reporting';
-
-        xpackMainPlugin.registerFeature({
-          id: featureId,
-          name: i18n.translate('reporting.featureRegistry.featureName', {
-            defaultMessage: 'reporting',
-          }),
-          navLinkId: featureId,
-          icon: 'questionInCircle',
-          app: [featureId, 'kibana'],
-          catalogue: [],
-          privileges: {
-            all: {
-              api: [],
-              savedObject: {
-                all: [],
-                read: [],
-              },
-              ui: ['show'],
-            },
-            read: {
-              api: [],
-              savedObject: {
-                all: [],
-                read: [],
-              },
-              ui: ['show'],
-            },
-          },
-        });
-      }
 
       // Add server routes and initialize the plugin here
       ReportingRoute(server, options, services);
